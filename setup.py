9ba09a07d328d0ca3cf9ad56fccdf70fc96e9726
furkan <furkan.dolek@cern.ch>
2020-01-29 17:13:46 +0100

    ds env

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Setup file for pypuca.
+    Use setup.cfg to configure your project.
+
+"""
+import sys
+
+from pkg_resources import VersionConflict, require
+from setuptools import setup
