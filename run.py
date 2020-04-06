78ad8a3a994d797508b4e227418fe3d5c35b6615
furkan <furkan.dolek@cern.ch>
2020-04-07 00:58:12 +0200

    event selected

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-import pandas as pd
 import os
-import numpy as np
-import pickle
 import time
+import pickle
+import numpy as np
+import pandas as pd
+# import numpy.ma as ma
+from scipy import stats
 from optparse import OptionParser
-import numpy.ma as ma
 
 start_time = time.time()
 
@@ -42,52 +43,49 @@ for filename in sorted(os.listdir(path_name + options.dataset)):
 
 bline_dict = {}
 
+print('baseline caculation')
 for key, value in data_dict.items():
     baseline = data_dict[key][0:data_dict[key].size, 1:100].mean(axis=1)
-    print(data_dict[key].size)
+    # print(data_dict[key].size)
     Nevent = (baseline.size)
-    print('nevent', Nevent)
+    # print(/'nevent', Nevent)
     blines = baseline.reshape(baseline.size, 1)
     data = (data_dict[key] - blines)
-
-    satur = ma.masked_greater(data, 14000)
-    sdata = np.ma.compress_rowcols(satur, 0)
-
-    linreg_array = np.array([])
-    for row in sdata[:, 390:420]:
-        y = row
-        x = np.arange(len(row))
-
-        slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-        trend= r_value * p_value
-
-        mask = [trend > 0 ]
-
-        linreg_array = np.append(linreg_array, mask, axis=0)
-        mask_reg1=linreg_array > 0
-
-    linreg_array = np.array([])
-    for row in sdata[:,390:420]:
-        y = row
-        x = np.arange(len(row))
-
-        slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-        trend = r_value * p_value
-        mask = [r_value > 0.6 ]
-
-        linreg_array  = np.append(linreg_array , mask, axis=0)
-        mask_reg2 = linreg_array  > 0
-    evtselect=sdata[mask_reg1*mask_reg2]
-    print(evtselect.shape)
-
-    # print(int(sdata.size/2000))
-    data = evtselect.mean(axis=0)
     # data1 = data.mean(axis=0)
     # print(data.max(axis=0))
     bline_dict.update({key: data})
 
-with open(options.dataset + '.pkl', 'wb') as fin:
-    pickle.dump(bline_dict, fin)
+del data_dict
+
+select_dict = {}
+
+maskflow = np.ones(10000) == True
+
+# print('test', maskflow.shape)
+print('Event selection')
+for key, value in bline_dict.items():
+
+    linreg_array = np.array([])
+    for row in value[:, 390:420]:
+        y = row
+        x = np.arange(len(row))
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+        trend = r_value * p_value
+
+        mask = [trend > 0]
+
+        linreg_array = np.append(linreg_array, mask, axis=0)
+        mask_reg1 = linreg_array > 0
+    maskflow = maskflow & mask_reg1
+
+for key, value in bline_dict.items():
+
+    select_dict.update({key: value[maskflow].mean(axis=0)})
+    print(key, value[maskflow].shape)
+del bline_dict
+with open(options.dataset + '.pkl.gz', 'wb') as fin:
+    pickle.dump(select_dict, fin)
 
 
 print("--- %.2f seconds ---" % (time.time() - start_time))
